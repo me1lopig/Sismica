c154eb1edfea1a93860301ea6b7f727ddd518dd3
Germán López Pineda <me1lopig@uco.es>
2023-03-26 12:24:55 +0200

    20230324

--- AceleracionCalculo.py
+++ AceleracionCalculo.py
@@ -5,19 +5,33 @@
 # numero de unidades
 n_unidades=int(input("Número de unidades ="))
 
+suma_espesores=0
+producto_c=0
+
+for i in range(0,n_unidades):
+    print("Unidad ",i+1)
+    espesor=float(input("Espesor de la unidad "))
+    c_parcial=float(input("Valor del coef. parcial C="))
+    producto_c+=espesor*c_parcial
+    suma_espesores+=espesor # comprobamos que la suma es 30
+
+# comprobaciones de los datos introducidos, espesores
+if (suma_espesores!=30):
+    print("La suma de los espesores no es de 30 m")
+    print("Compruebe los valores de entrada")
+    exit() # se sale del programa
+
 
 
 ab=float(input("Aceleración basica adimensional ab="))
 rho=float(input("Coeficinte de riesgo rho=[ 1 o 1.3]="))
-c=float (input("Coeficiente del terreno C="))
+c=producto_c/30 # promedio ponderado del coeficiente del terreno
 
 
 # Calculo del coficiente de amplificación del terreno
 producto=ab*rho
 
-print("Producto =",producto)
-
-# calculo según loa rangos
+# calculo según los rangos
 if (producto<=0.1):
     S=c/1.25
 elif (0.1<=producto<0.4):
